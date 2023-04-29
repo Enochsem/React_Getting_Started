0aa42d96aae0937c2db4c128276a68b1a5489220
Enoch Sem <enochsem@gmail.com>
2023-04-29 22:43:07 +0000

    loaded product detail dynamically

--- src/ProductDetail.js
+++ src/ProductDetail.js
@@ -2,11 +2,27 @@ import classes from './ProductDetail.module.css'
 // import ProductData from './ProductData';
 
 const ProductDetail = (props)=>{
+
+
   const colorOptions = props.data.colorOptions.map((item,pos)=>{
-    return (<img className={classes.ProductImage} src={item.imageUrl} key={pos} alt={item.styleName}/>); 
+    const classArrary = [classes.ProductImage];
+    if(pos === 0){
+      classArrary.push(classes.SelectedProductImage);
+    }
+    return (<img className={classArrary.join(' ')} src={item.imageUrl} key={pos} alt={item.styleName}/>); 
   });
-  // React_Getting_Started
-  // getting started on react projects
+  
+
+  const featureList = props.data.featureList.map((item, pos)=>{
+    const classArrary =[classes.FeatureItem]
+    if(pos === 0){classArrary.push(classes.SelectedFeatureItem);}
+    return (
+    <button key={pos} className={classArrary.join(' ')}>{item}</button>
+    );
+  });
+
+
+  
     return (
         <div className={classes.ProductData}>
           <h1 className={classes.ProductTitle}>{props.data.title}</h1>
@@ -14,15 +30,14 @@ const ProductDetail = (props)=>{
 
           <h3 className={classes.SectionHeading}>Select Color</h3>
           <div className={classes.SectionBlock}>
-            <img className={[classes.ProductImage, classes.SelectedProductImage].join(' ')} src='https://imgur.com/iOeUBV7.png'/>
+            {/* <img className={[classes.ProductImage, classes.SelectedProductImage].join(' ')} src='https://imgur.com/iOeUBV7.png'/> */}
             {colorOptions}
             
           </div>
 
           <h3 className={classes.SectionHeading}>Features</h3>
           <div className={classes.SectionBlock}>
-            <button className={[classes.FeatureItem , classes.SelectedFeatureItem].join(' ')}>Time</button>
-            <button className={classes.FeatureItem}>Heart Rate</button>
+            {featureList}
           </div>
           <br/>
           <button className={[classes.FeatureItem, classes.PrimaryButton].join(' ')}>Buy Now</button>
